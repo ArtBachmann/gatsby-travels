743783ff84cfa8dfe69698876be961d3b370fd48
Art Bachmann <bachmann.art@gmail.com>
2019-07-01 16:17:45 +0300

    environment variables added

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,6 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
 
 module.exports = {
   siteMetadata: {
@@ -19,10 +17,27 @@ module.exports = {
         path: `${__dirname}/src/images/`,
       },
     },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        // Learn about environment variables: https://gatsby.dev/env-vars
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    },
+    // {
+    //   resolve: `gatsby-source-contentful`,
+    //   options: {
+    //     spaceId: "det1h2iypfuq",
+    //     // Learn about environment variables: https://gatsby.dev/env-vars
+    //     accessToken: "kXal8pld0dYV3G0amgQ1xnAZfuoWyaPiNWtfLE9k2J8",
+    //   },
+    // },
     "gatsby-plugin-sass",
     "gatsby-plugin-styled-components",
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     "gatsby-plugin-transition-link",
+    "gatsby-plugin-playground",
   ],
 }
